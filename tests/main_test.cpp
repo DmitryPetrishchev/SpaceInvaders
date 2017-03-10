a540659a820e3705422e01e8e1eb3c99178fb2e7
DmitryPetrishchev <dimonozersk@ya.ru>
2017-03-10 20:34:49 +0300

    .gitignoe, CMakeLists, src files, tets files, data files were added

---
+++ tests/main_test.cpp
@@ -0,0 +1,18 @@
+#include "gtest/gtest.h"
+
+#include <string>
+
+#include "GameObjects/GameObjects.hpp"
+#include "GameContext.hpp"
+#include "Tools/Logger/Logger.hpp"
+#include "Patterns/Factory.hpp"
+
+TEST(startA, main_testA)
+{
+  Player.do_smth();
+  EXPECT_EQ(Player.m_i, 2);
+  Log(Logger::Info) << std::string("msg");
+
+  Context.SetStrategy(GameStrategyType::Around);
+  EXPECT_EQ(Context.GetStrategy(), GameStrategyType::Around);
+}
